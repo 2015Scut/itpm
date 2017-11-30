88be240137722f0c76bed7205a68625847160763
Hegs30 <1183643895@qq.com>
2017-11-30 19:48:48 +0800

    version1.0

--- src/model/MajorProcess.java
+++ src/model/MajorProcess.java
@@ -1,12 +1,79 @@
 package model;
 
 
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ * 处理分科类数据的类，可以获取和插入分科信息
+ * @author 邢浩
+ * @version 1.0
+ * 
+ */
 
 public class MajorProcess implements Process{
 
+	private Connection ct=null;
+	private PreparedStatement ps=null;
+	private ResultSet rs=null;
+	/**插入数据的sql语句*/
+	private static final String insertSQL="insert into major values (?,?,?)";
+	/**根据一个值搜索出的分科集合sql语句*/
+	private static final String searchSQL="select *from major where grade_id=?";
 	public MajorProcess() {}
-
-	
-
+	/**
+	 * 向数据库插入用户
+	 * @param major_id 分科的id
+	 * @param major_name 分科的名字
+	 * @param grade_id 年级id
+	 * @return 是否成功
+	 * @throws SQLException SQL异常
+	 */
+	public boolean insertmajor(String major_id,String major_name,int grade_id) throws SQLException {
+		ct=ConnDB.getConn();//获取数据库连接
+		ps=ct.prepareStatement(searchSQL);//验证majorid是否重复
+		ps.setString(1, major_id);
+		rs=ps.executeQuery();
+		System.out.println(ps);
+		if(rs.next())return false;
+		
+		ps=ct.prepareStatement(insertSQL);//插入学生表
+		ps.setString(1, major_id);//向String中？的地方填入数据
+		ps.setString(2,major_name);
+		ps.setInt(3, grade_id);
+		System.out.println(ps);
+		ps.executeUpdate();
+		
+		ct.close();//关闭数据库，记得每次用完都要关闭
+		ct=null;
+		ps=null;
+		rs=null;
+		return true;
+	}
+	public ResultSet getData(int grade_id) throws SQLException
+	{
+		ct=ConnDB.getConn();
+		ps=ct.prepareStatement(searchSQL);
+		ps.setInt(1,grade_id);
+		ps.executeQuery();
+		ct.close();
+		ct=null;
+		ps=null;
+		return rs;
+	}
+	/**
+	 * 测试函数
+	 * @param args 参数
+	 */
+	public static void main(String [] args) {//测试
+		MajorProcess up=new MajorProcess();
+		try {
+			up.insertmajor("00002","理科",2016);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
