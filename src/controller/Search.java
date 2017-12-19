8982c30bb9743b0dfda9e88a46ed70b76fcce038
123 <123@DESKTOP-VUEDUG7>
2017-12-19 20:18:11 +0800

    0

--- src/controller/Search.java
+++ src/controller/Search.java
@@ -6,7 +6,7 @@ import model.*;
 
 public class Search {
 	public static ArrayList<Integer> getGrade(){
-		//查询数据库中年级列表 龙康
+		//查询数据库中年级列表  龙康
 		GradeProcess gp=new GradeProcess();
 		ArrayList<Grade> grade = gp.getData();
 		ArrayList<Integer> gradeid = new ArrayList<Integer>();
@@ -17,7 +17,7 @@ public class Search {
 		return gradeid;
 	}
 	public static ArrayList<String> getClasses(Integer g,String m){
-		//根据年级和专业查询班级列表 龙康
+		//根据年级和专业查询班级列表  龙康
 		ClassesProcess cp=new ClassesProcess();
 		ArrayList<Classes> Classes_ = cp.getData(m, g);
 		ArrayList<String> Classname = new ArrayList<String>();
