fa2244ea4572e570daa6ed85bb9054a1a5d6d3c5
swifthao <1525901124@qq.com>
2017-12-20 23:19:07 +0800

    插入函数

--- src/controller/Insert.java
+++ src/controller/Insert.java
@@ -1,4 +1,8 @@
 package controller;
+import model.ClassesProcess;
+import model.MajorProcess;
+import model.StudentProcess;
+import model.TeacherProcess;
 import view.*;
 
 
@@ -12,15 +16,21 @@ public class Insert {
 	
 	public static String addStudent(Integer g,String m,String c,String sid,String name,String sex) {
 		//插入学生 邢浩
+		StudentProcess sp=new StudentProcess();
+		sp.insertStudent(sid, name, g, sex, null, c, m, g);
 		return null;
 	}
 	
 	public static String addClass(Integer g,String m,String cid,String cname,String teacherName) {
 		//邢浩
+		ClassesProcess cp=new ClassesProcess();
+		cp.insertClasses(cid, cname, teacherName);
 		return null;
 	}
 	public static String addTeacher(Integer g,String tid,String name,String sex) {
 		//邢浩
+		TeacherProcess tp=new TeacherProcess();
+		tp.insertTeacher(tid, name, g, sex, g);
 		return null;
 	}
 }
